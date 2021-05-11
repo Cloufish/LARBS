71d27ad4291228959cd3295cde77718d5dc51a35
Cloufish <bartmok17@gmail.com>
2021-05-11 12:13:32 +0200

    Added my own script

---
+++ after-larbs.sh
@@ -0,0 +1,17 @@
+# Learning Folders
+mkdir Learning && cd Learning || echo {Failure in cd}
+git clone https://github.com/Cloufish/LearningProjects.git
+cd ..
+mkdir Projects && cd Projects || echo {Failure in cd}
+git clone https://github.com/Cloufish/blackarch-zsh-container
+git clone https://github.com/Cloufish/LARBS.git
+git clone https://github.com/Cloufish/voidrice.git
+git clone https://github.com/Cloufish/bug_bounty_framework
+git clone https://github.com/Cloufish/Awesome-Worldwide-ITSecurity-Specialists.git
+git clone https://github.com/Cloufish/Cloufish.git
+git clone https://github.com/Cloufish/vimwiki.git
+cd ~/ || echo {Failure in cd}
+mkdir Pentesting && cd Pentesting || echo {Failure in cd}
+mkdir BugBountyPrograms VulnerableLabs CTFs VPNs
+cd ~/ || echo {Failure in cd}
+git clone https://github.com/Cloufish/blog.git
