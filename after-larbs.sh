31e0ec34c7decaae1672dc72dfac5038876696ac
Cloufish <32965886+Cloufish@users.noreply.github.com>
2021-05-15 12:46:08 +0200

    Update after-larbs.sh

--- after-larbs.sh
+++ after-larbs.sh
@@ -1,8 +1,8 @@
 # Learning Folders
-mkdir Learning && cd Learning || echo {Failure in cd}
+mkdir Learning && cd Learning || { echo "Failure in cd command "; exit 1; }
 git clone https://github.com/Cloufish/LearningProjects.git
 cd ..
-mkdir Projects && cd Projects || echo {Failure in cd}
+mkdir Projects && cd Projects || { echo "Failure in cd command "; exit 1; }
 git clone https://github.com/Cloufish/blackarch-zsh-container
 git clone https://github.com/Cloufish/LARBS.git
 git clone https://github.com/Cloufish/voidrice.git
@@ -10,11 +10,11 @@ git clone https://github.com/Cloufish/bug_bounty_framework
 git clone https://github.com/Cloufish/Awesome-Worldwide-ITSecurity-Specialists.git
 git clone https://github.com/Cloufish/Cloufish.git
 git clone https://github.com/Cloufish/vimwiki.git
-cd ~/ || echo {Failure in cd}
-mkdir Pentesting && cd Pentesting || echo {Failure in cd}
+cd ~/ || { echo "Failure in cd command "; exit 1; }
+mkdir Pentesting && cd Pentesting { echo "Failure in cd command "; exit 1; }
 mkdir BugBountyPrograms VulnerableLabs CTFs VPNs
-cd ~/ || echo {Failure in cd}
+cd ~/ || { echo "Failure in cd command "; exit 1; }
 git clone https://github.com/Cloufish/blog.git
 
-cd ~/ || echo {Failure in cd}
+cd ~/ || { echo "Failure in cd command "; exit 1; }
 ln -s ~/.config/zsh/.zshenv ~/.zshenv
