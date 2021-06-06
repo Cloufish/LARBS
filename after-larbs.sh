1caac5021fe6cebb64b12efdc54d62ca0b0bbf0e
Cloufish <32965886+Cloufish@users.noreply.github.com>
2021-06-06 19:08:45 +0200

    Update after-larbs.sh

--- after-larbs.sh
+++ after-larbs.sh
@@ -3,10 +3,10 @@ mkdir Learning && cd Learning || { echo "Failure in cd command "; exit 1; }
 git clone https://github.com/Cloufish/LearningProjects.git
 cd ..
 mkdir Projects && cd Projects || { echo "Failure in cd command "; exit 1; }
-git clone https://github.com/Cloufish/blackarch-zsh-container
+git clone https://github.com/Markdown-Bug-Bounty-Recon/Docker-Containers.git
 git clone https://github.com/Cloufish/LARBS.git
 git clone https://github.com/Cloufish/voidrice.git
-git clone https://github.com/Cloufish/bug_bounty_framework
+git clone https://github.com/Markdown-Bug-Bounty-Recon/Markdown-Bug-Bounty-Recon.git
 git clone https://github.com/Cloufish/Awesome-Worldwide-ITSecurity-Specialists.git
 git clone https://github.com/Cloufish/Cloufish.git
 git clone https://github.com/Cloufish/vimwiki.git
