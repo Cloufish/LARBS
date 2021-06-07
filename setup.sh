bbb2562fef9fba5e6170faad4ebef48e7c5c5eff
Cloufish <32965886+Cloufish@users.noreply.github.com>
2021-06-07 10:21:21 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 wget https://raw.githubusercontent.com/Cloufish/LARBS/master/larbs.sh
 chmod +x larbs.sh
-./larbs.sh -r https://github.com/Cloufish/voidrice -p https://raw.githubusercontent.com/Cloufish/LARBS/master/progs.csv
+sudo ./larbs.sh -r https://github.com/Cloufish/voidrice -p https://raw.githubusercontent.com/Cloufish/LARBS/master/progs.csv
 
 wget https://raw.githubusercontent.com/Cloufish/LARBS/master/after-larbs.sh
 chmod +x after-larbs.sh
