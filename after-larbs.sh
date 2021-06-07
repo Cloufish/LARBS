596e59400fc8e6a69ef369c6f4fe1c548488bb82
Cloufish <cloufish@protonmail.com>
2021-06-07 13:00:54 +0200

    Updated

--- after-larbs.sh
+++ after-larbs.sh
@@ -4,12 +4,20 @@ git clone https://github.com/Cloufish/LearningProjects.git
 cd ..
 mkdir Projects && cd Projects || { echo "Failure in cd command "; exit 1; }
 git clone https://github.com/Markdown-Bug-Bounty-Recon/Docker-Containers.git
-git clone https://github.com/Cloufish/LARBS.git
-git clone https://github.com/Cloufish/voidrice.git
 git clone https://github.com/Markdown-Bug-Bounty-Recon/Markdown-Bug-Bounty-Recon.git
 git clone https://github.com/Cloufish/Awesome-Worldwide-ITSecurity-Specialists.git
 git clone https://github.com/Cloufish/Cloufish.git
 git clone https://github.com/Cloufish/vimwiki.git
+
+mkdir Configs && cd Configs || { echo "Failure in cd command "; exit 1; }
+git clone https://github.com/Cloufish/LARBS.git
+git clone https://github.com/Cloufish/voidrice.git
+git clone https://github.com/Cloufish/dwmblocks.git
+git clone https://github.com/Cloufish/dmenu.git
+git clone https://github.com/Cloufish/st.git
+git clone https://github.com/Cloufish/dwm.git
+
+
 cd ~/ || { echo "Failure in cd command "; exit 1; }
 mkdir Pentesting && cd Pentesting { echo "Failure in cd command "; exit 1; }
 mkdir BugBountyPrograms VulnerableLabs CTFs VPNs
@@ -17,4 +25,6 @@ cd ~/ || { echo "Failure in cd command "; exit 1; }
 git clone https://github.com/Cloufish/blog.git
 
 cd ~/ || { echo "Failure in cd command "; exit 1; }
-ln -s ~/.config/zsh/.zshenv ~/.zshenv
+
+
+sudo archlinux-java set java-11-openjdk
--- setup.sh
+++ setup.sh
@@ -4,6 +4,4 @@ wget https://raw.githubusercontent.com/Cloufish/LARBS/master/larbs.sh
 chmod +x larbs.sh
 ./larbs.sh -r https://github.com/Cloufish/voidrice -p https://raw.githubusercontent.com/Cloufish/LARBS/master/progs.csv
 
-wget https://raw.githubusercontent.com/Cloufish/LARBS/master/after-larbs.sh
-chmod +x after-larbs.sh
-./after-larbs.sh
+## Now get your Git Credentials... and then run after-larbs.sh
