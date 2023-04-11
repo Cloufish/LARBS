52c96171870ee885b3f6e335eb46750ee487732e
Cloufish <32965886+Cloufish@users.noreply.github.com>
2023-04-11 18:17:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ fi
 
 wget https://raw.githubusercontent.com/Cloufish/LARBS/master/larbs.sh
 chmod +x larbs.sh
-sudo ./larbs.sh -r https://github.com/Cloufish/voidrice -p https://raw.githubusercontent.com/Cloufish/LARBS/master/progs.csv
+sudo ./larbs.sh -r https://github.com/Cloufish/dotfiles -p https://raw.githubusercontent.com/Cloufish/LARBS/master/progs.csv
 
 ## Now get your Git Credentials... and then run after-larbs.sh
