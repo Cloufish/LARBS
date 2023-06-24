603a17132f7608824d902f251b41c4250b634d00
Cloufish <32965886+Cloufish@users.noreply.github.com>
2023-06-24 17:20:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-wget https://raw.githubusercontent.com/Cloufish/LARBS/master/larbs.sh
 chmod +x larbs.sh
 sudo ./larbs.sh -r https://github.com/Cloufish/dotfiles -p https://raw.githubusercontent.com/Cloufish/LARBS/master/progs.csv
 
